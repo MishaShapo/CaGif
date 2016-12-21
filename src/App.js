057a6114f8011894f0a1f743df1d578def2c8992
Michail Shaposhnikov <misha.shapo98@gmail.com>
2016-12-21 15:25:58 -0600

    Clean Slate
    Inited new react native project and refactored graphics

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{margin:128}}>Hello, CaGif! =D </Text>
+      </View>
+    )
+  }
+}
+
+export default App;
