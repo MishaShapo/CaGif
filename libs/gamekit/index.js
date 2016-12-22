1388a02085919bac0da6664fb3e190c98da8091f
Michail Shaposhnikov <misha.shapo98@gmail.com>
2016-12-21 19:01:12 -0600

    Basic sprite working
    Loaded up react-game-kit, found proper components (the library is for
    both react and react native and the native components were located in
    the ’react-game-kit/native’ directory). Basic animations are working.
    Next step is to get sprite to respond to touch events

---
+++ libs/gamekit/index.js
@@ -0,0 +1,19 @@
+import AudioPlayer from './utils/Audio-player.js';
+import Body from './components/Body.js';
+import Loop from './components/Loop.js';
+import KeyListener from './utils/Key-listener.js';
+import Sprite from './components/Sprite.js';
+import Stage from './components/Stage.js';
+import TileMap from './components/Tile-map.js';
+import World from './components/World.js';
+
+export {
+  AudioPlayer,
+  Body,
+  Loop,
+  KeyListener,
+  Sprite,
+  Stage,
+  TileMap,
+  World,
+};
--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Image, Dimensions, Text } from 'react-native';
+import { Loop, Stage, Sprite } from 'react-game-kit/native';
 
 class App extends Component {
   render() {
+    const { width, height } = Dimensions.get('window');
     return (
-      <View>
-        <Text style={{margin:128}}>Hello, CaGif! =D </Text>
-      </View>
-    )
+        <Loop style={styles.loopStyle}>
+          <Stage style={styles.stageStyle} width={width} height={height}>
+            <Image
+              source={require('../graphics/background.png')}
+              style={styles.bgStyle}
+            >
+              <Sprite
+                src={require("../graphics/spritesheets/cat_spritesheet.png")}
+                tileHeight={185}
+                tileWidth={210}
+                steps={[9,9,7,9]}
+                state={1}
+                repeat
+                ticksPerFrame={5}
+              />
+            </Image>
+          </Stage>
+        </Loop>
+    );
+  }
+}
+
+const styles = {
+  loopStyle: {
+    flex: 1
+  },
+  stageStyle: {
+    flex: 1,
+    position: 'relative'
+  },
+  bgStyle: {
+    flex: 1,
+    position: 'relative',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 }
 
